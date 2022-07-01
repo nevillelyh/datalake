d95d41a98370c61ab48eb811e6224425f6031627
Neville Li <neville.lyh@gmail.com>
2022-07-01 10:05:05 -0400

    make mysql-init idempotent

--- scripts/mysql-init.sql
+++ scripts/mysql-init.sql
@@ -1,14 +1,14 @@
 -- Metastores
-CREATE DATABASE hive;
-CREATE DATABASE iceberg;
+CREATE DATABASE IF NOT EXISTS hive;
+CREATE DATABASE IF NOT EXISTS iceberg;
 
-CREATE USER 'hive'@'%' IDENTIFIED BY 'hivepass';
-CREATE USER 'iceberg'@'%' IDENTIFIED BY 'icebergpass';
+CREATE USER IF NOT EXISTS 'hive'@'%' IDENTIFIED BY 'hivepass';
+CREATE USER IF NOT EXISTS 'iceberg'@'%' IDENTIFIED BY 'icebergpass';
 
 GRANT ALL PRIVILEGES ON hive.* TO 'hive'@'%';
 GRANT ALL PRIVILEGES ON iceberg.* TO 'iceberg'@'%';
 
 -- MySQL catalog
-CREATE DATABASE iris;
-CREATE USER 'trino'@'%' IDENTIFIED BY 'trinopass';
+CREATE DATABASE IF NOT EXISTS iris;
+CREATE USER IF NOT EXISTS 'trino'@'%' IDENTIFIED BY 'trinopass';
 GRANT ALL PRIVILEGES ON iris.* TO 'trino'@'%';
