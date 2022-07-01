94507f693480d91f4304f6503dd1d6117a909d6e
Neville Li <neville.lyh@gmail.com>
2022-07-01 10:24:17 -0400

    make mc idempotent

--- scripts/minio.sh
+++ scripts/minio.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 cd "$HOME"
 scripts/wait-for-it.sh "minio.datalake.io:9000" -s -t 0 -- echo "MinIO is up"
+
 mc alias set minio http://minio.datalake.io:9000 minioadmin minioadmin
-mc mb minio/datalake
-echo -e "datalake\ndatalakepass" | mc admin user add minio datalake
-mc admin user svcacct add --access-key datalake-key --secret-key datalake-secret minio datalake
-mc admin policy set minio readwrite user=datalake
+if ! mc ls minio/datalake &> /dev/null; then
+    mc mb minio/datalake
+    echo -e "datalake\ndatalakepass" | mc admin user add minio datalake
+    mc admin user svcacct add --access-key datalake-key --secret-key datalake-secret minio datalake
+    mc admin policy set minio readwrite user=datalake
+fi
