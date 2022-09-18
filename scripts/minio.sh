b4707b0666391b7f07218f9a0248858a35c18d2e
Neville Li <neville.lyh@gmail.com>
2022-09-18 16:15:32 -0400

    simplify minio setup

--- scripts/minio.sh
+++ scripts/minio.sh
@@ -8,7 +8,6 @@ scripts/wait-for-it.sh "minio.datalake.io:9000" -s -t 0 -- echo "MinIO is up"
 mc alias set minio http://minio.datalake.io:9000 minioadmin minioadmin
 if ! mc ls minio/datalake &> /dev/null; then
     mc mb minio/datalake
-    echo -e "datalake\ndatalakepass" | mc admin user add minio datalake
-    mc admin user svcacct add --access-key datalake-key --secret-key datalake-secret minio datalake
-    mc admin policy set minio readwrite user=datalake
+    mc admin user add minio datalake-key datalake-secret
+    mc admin policy set minio readwrite user=datalake-key
 fi
